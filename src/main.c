2fe0acb66c991eefc7f77723e44b694b552cf47d
Douglas Chidester <tikiman92@gmail.com>
2015-11-26 13:51:54 -0800

    overall flow of main written in comments

--- src/main.c
+++ src/main.c
@@ -2,14 +2,25 @@
 
 int main(int argc, char* argv[])
 {
+	// init
 	int userNumber = 0;
+	int result = 0;
+	memset(tableInt, 0, TABLE_SIZE);
 
-	memset(table, 0, TABLE_SIZE);
+	// TODO check argc, argv
 
-	puts("Enter a value:");
+	// TODO start loop
+	
+	puts("Enter a value (-1 to quit):");
 	scanf("%d", &userNumber);
-
-	printf("%d! = %d\n", userNumber, factorialInt(userNumber));
+	// TODO start timer
+	result = factorialInt(userNumber);
+	// TODO end timer
+	printf("%d! = %d\n", userNumber, result); // add time taken to compute
+	
+	// TODO end loop
+	
+	
 	//printf("%.2f! = %.2f\n", userNumber, factorialF(userNumber));
 
 	return 0;
